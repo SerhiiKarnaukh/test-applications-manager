ad6db24850cbb6506e6ed72ac91b5437637ed9e7
SerhiiKarnaukh <serhiikarnaukh1984@gmail.com>
2023-03-22 11:14:05 +0200

    Changed SMTP configuration

--- shop/settings.py
+++ shop/settings.py
@@ -170,8 +170,8 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = '#'
-EMAIL_PORT = '#'
-EMAIL_HOST_USER = '#'
-EMAIL_HOST_PASSWORD = '#'
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = bool(int(os.environ.get("EMAIL_USE_TLS", default=1)))
