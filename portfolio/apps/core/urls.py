8560cf53a483bf0cab87edb2c4fd9d677171bae3
SerhiiKarnaukh <serhiikarnaukh1984@gmail.com>
2023-04-13 11:44:55 +0300

    Finished TAM frontend

--- portfolio/apps/core/urls.py
+++ portfolio/apps/core/urls.py
@@ -1,17 +1,8 @@
 from django.urls import path
 
-from core.views import index
+from .views import index, app_detail
 
 urlpatterns = [
     path('', index, name='index'),
-    # path('', CategoryDetail.as_view(), name='store'),
-    # path('contact/', contact, name='contact'),
-    # path('about/', about, name='about'),
-    # path('category/<slug:slug>/',
-    #      CategoryDetail.as_view(),
-    #      name='category_detail'),
-    # path('category/<slug:category_slug>/<slug:slug>/',
-    #      ProductDetail.as_view(),
-    #      name='product_detail'),
-    # path('search/', ProductSearchListView.as_view(), name='search'),
+    path('app/', app_detail, name='app_detail')
 ]
--- portfolio/apps/core/views.py
+++ portfolio/apps/core/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+
+def app_detail(request):
+    return render(request, 'core/portfolio_detail.html')
