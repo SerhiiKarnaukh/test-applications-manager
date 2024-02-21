6780d7f46612cef1a07166722e568d4f68385961
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2024-02-21 20:53:11 +0200

    Add: social profile edit password

--- portfolio/apps/social_profiles/api.py
+++ portfolio/apps/social_profiles/api.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.contrib.auth.forms import PasswordChangeForm
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -140,3 +141,17 @@ def editprofile(request):
             })
 
         return JsonResponse({'message': 'Invalid form data!'})
+
+
+@api_view(['POST'])
+def editpassword(request):
+    user = request.user
+
+    form = PasswordChangeForm(data=request.POST, user=user)
+
+    if form.is_valid():
+        form.save()
+
+        return JsonResponse({'message': 'success'})
+    else:
+        return JsonResponse({'message': form.errors.as_json()}, safe=False)
--- portfolio/apps/social_profiles/urls.py
+++ portfolio/apps/social_profiles/urls.py
@@ -7,6 +7,7 @@ app_name = 'social_profiles'
 urlpatterns = [
     path('register/', api.SocialProfileCreateView.as_view(), name='user-register'),
     path('editprofile/', api.editprofile, name='editprofile'),
+    path('editpassword/', api.editpassword, name='editpassword'),
     path('friends/<slug:slug>/', api.friends, name='friends'),
     path('friends/<slug:slug>/request/',
          api.send_friendship_request,
