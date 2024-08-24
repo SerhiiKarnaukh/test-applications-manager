ea1bbb888577a83123c51d2d931940bb04fb3d17
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2024-08-24 10:23:54 +0300

    Fix: create_social_posts_trends task

--- portfolio/apps/social_posts/tasks.py
+++ portfolio/apps/social_posts/tasks.py
@@ -3,37 +3,37 @@ from django.utils import timezone
 from datetime import timedelta
 from collections import Counter
 import re
+from django.db.models import Q
 
 from .models import Post, Trend
 
 
 @shared_task
 def create_social_posts_trends():
-    def extract_hashtags(text, trends):
-        for word in text.split():
-            if len(word) > 1 and word[0] == '#':
-                hashtag = word[1:].lower()
-                hashtag = re.sub(r'\W+$', '', hashtag)
-                trends.append(hashtag)
-
-        return trends
-
-    for trend in Trend.objects.all():
-        trend.delete()
-
-    trends = []
     this_hour = timezone.now().replace(minute=0, second=0, microsecond=0)
     twenty_four_hours = this_hour - timedelta(hours=24)
 
-    for post in Post.objects.filter(created_at__gte=twenty_four_hours).filter(is_private=False):
-        extract_hashtags(post.body, trends)
+    Trend.objects.all().delete()
 
-    if not trends:
-        all_posts = Post.objects.filter(is_private=False).order_by('-created_at')
-        for post in all_posts:
-            extract_hashtags(post.body, trends)
-            if len(trends) >= 5:
-                break
+    posts = Post.objects.filter(created_at__gte=twenty_four_hours, is_private=False)
 
-    for trend in Counter(trends).most_common(10):
-        Trend.objects.create(hashtag=trend[0], occurences=trend[1])
+    if not posts.exists():
+        posts = Post.objects.filter(is_private=False).order_by('-created_at')[:5]
+
+    trends_counter = Counter()
+
+    for post in posts:
+        for word in post.body.split():
+            if len(word) > 1 and word[0] == '#':
+                hashtag = word[1:].lower()
+                hashtag = re.sub(r'\W+$', '', hashtag)
+                hashtag_pattern = r'#' + re.escape(hashtag) + r'(\b|[^a-zA-Z0-9_])'
+
+                occurences = Post.objects.filter(
+                    Q(is_private=False) & Q(body__iregex=hashtag_pattern)
+                ).count()
+
+                trends_counter[hashtag] = occurences
+
+    for hashtag, occurences in trends_counter.most_common(10):
+        Trend.objects.create(hashtag=hashtag, occurences=occurences)
