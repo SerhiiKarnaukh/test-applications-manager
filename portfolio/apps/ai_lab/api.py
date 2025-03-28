ab590c549443cdfe5d97bcc4876072f61549a35d
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2025-03-28 11:22:02 +0200

    [ai_lab] - added test chat

--- portfolio/apps/ai_lab/api.py
+++ portfolio/apps/ai_lab/api.py
@@ -1,10 +1,23 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
+from django.conf import settings
+from openai import OpenAI
 
 
 class AiLabTestView(APIView):
     permission_classes = [AllowAny]
 
-    def get(self, request):
-        return Response({"message": "Welcome to AI Lab API!"})
+    def post(self, request):
+        question = request.data.get("question")
+        api_key = settings.OPENAI_API_KEY
+        client = OpenAI(api_key=api_key)
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{
+                "role": "user",
+                "content": question
+            }]
+        )
+
+        return Response({"message": completion.choices[0].message.content})
