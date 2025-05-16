b7091cdca23ed4c2e63a1feef162ae4e7673de68
SerhiiKarnaukh <serhiikarnaukh.cn@gmail.com>
2025-05-16 18:03:06 +0300

    [core] - fixed gunicorn settings

--- docker/proxy/nginx/default-ssl.conf.tpl
+++ docker/proxy/nginx/default-ssl.conf.tpl
@@ -29,7 +29,7 @@ server {
     }
 
     location / {
-        nclude               gunicorn_headers;
+        include              gunicorn_headers;
         proxy_redirect       off;
         proxy_pass           http://${APP_HOST}:${APP_PORT};
         client_max_body_size 75M;
--- scripts/run.sh
+++ scripts/run.sh
@@ -11,5 +11,5 @@ python manage.py wait_for_db
 python manage.py collectstatic --noinput
 python manage.py migrate
 
-gunicorn --bind 0.0.0.0:9000 gst.wsgi --workers 2 --threads 4 &
-gunicorn --bind 0.0.0.0:8000 gst.asgi -w 1 --threads 4 -k uvicorn.workers.UvicornWorker
+gunicorn --bind 0.0.0.0:9000 portfolio.wsgi --workers 1 --threads 4 &
+gunicorn --bind 0.0.0.0:8000 portfolio.asgi -w 1 --threads 4 -k uvicorn.workers.UvicornWorker
